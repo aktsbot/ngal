fa2cafc3dbb03e1efa75437b64e0602fa29f73a5
akts <box.ashishk@gmail.com>
2019-01-10 00:43:38 +0530

    I forgot to change the HTML template's title tag... Time to pack it up brah!

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ const buildHTML = () => {
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-  <title>Simple Gallery</title>
+  <title>ngal: A simple image gallery</title>
   <style>
     div {
       display: flex;
